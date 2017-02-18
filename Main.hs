4cc8064fb1f57b81a1389fb6cc62ed20548b39dc
Dave Compton <davecompton7@gmail.com>
2017-02-18 02:33:56 +0000

    alternating red/blue now

--- Main.hs
+++ Main.hs
@@ -2,8 +2,22 @@
 {-# LANGUAGE RecursiveDo #-}
 import Reflex
 import Reflex.Dom
+import Data.Map
+import Data.Text
+import Data.Time.Clock (getCurrentTime)
+import Control.Monad.Trans (liftIO)
 
-main = mainWidget $ mdo
-         (element, _) <- el' "h1" $ dynText $ fmap (\n -> if n > 0 then "Clicked" else "Hello World") nClicks
-         nClicks <- foldDyn (\_ -> succ) 0 $ domEvent Click element 
-         el "p" $ text "This is my test document."
+
+webPage :: MonadWidget t m => m ()
+webPage = do
+  now <- liftIO getCurrentTime 
+  ticker <- tickLossy 1.0 now
+  counter <- foldDyn  (\_ n -> n+1) (0::Integer) ticker
+  let redBlue = fmap (\n -> if ((n `mod` 2) == 0) then "red" else "blue") $ counter
+  let redBlueStyle = fmap (\color -> (fromList [(pack "style",pack $ "color: " ++ color)])) redBlue
+  elDynAttr' "h1"  (constDyn $ fromList [("style","color: blue")]) $ text "Hello World!"
+  elDynAttr' "p"  redBlueStyle $ text "This is my test document"
+  return ()
+
+main :: IO ()
+main = mainWidgetWithCss "h1 {font-family: Helvetica;} p {font-family: Helvetica;}" webPage
